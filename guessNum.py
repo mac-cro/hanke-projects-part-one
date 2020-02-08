010f4f4786527664f7e97310755c59ba86b818a1
red-hanke <60523481+red-hanke@users.noreply.github.com>
2020-02-07 19:27:12 -0500

    Create guessNum.py
    added range cap prompt

---
+++ guessNum.py
@@ -0,0 +1,30 @@
+import random, sys
+
+max = int(input("Max Number: "))
+print('The number is between 1 and %s.'% max)
+
+correct = random.choice(range(1,max))
+# print("The number is %s." % correct)
+
+def guessNum():
+    # global correct
+
+    guess = int(input('Guess Number: '))
+    if guess > max:
+        print('Error: exceeding max!\n')
+        guessNum()
+
+    if guess != correct:
+        if guess > correct:
+            print("Too high!\n")
+            guessNum()
+
+        elif guess < correct:
+            print("Too low!\n")
+            guessNum()
+            
+    elif guess == correct:
+        print("You've guessed it!\n")
+        sys.exit()
+
+guessNum()
